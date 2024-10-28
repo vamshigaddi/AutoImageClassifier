69d9da1590b94e726ef049c3acdb7ce4680767dd
vamshi gaddi <144581498+vamshigaddi@users.noreply.github.com>
2024-10-28 09:58:45 +0530

    Update s.py

--- image_classifier/images/s.py
+++ image_classifier/images/s.py
@@ -4,8 +4,8 @@ import os
 
 
 # Initialize Supabase client (adjust with your actual URL and key)
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://rdhasvphabwzstdfiwkz.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkaGFzdnBoYWJ3enN0ZGZpd2t6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMzU0MzAwOSwiZXhwIjoyMDM5MTE5MDA5fQ.7toLAIqt65kMV-xs6_e7upBrvmEckNpiHgipMLmX8do")
+SUPABASE_URL = os.getenv("SUPABASE_URL", "url")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "key")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 #user = supabase.auth.sign_up({ "email": 'vamshigaddi.da@gmail.com', "password": 'Bruslee555555'})
